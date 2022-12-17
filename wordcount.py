34d81ef4ddb1ec486a82ea58812ab668ca518adc
Maryam Abdulle <iammaryamabdulle@gmail.com>
2022-12-16 22:15:18 -0700

    Added more features on the further-study file

--- wordcount.py
+++ wordcount.py
@@ -1,21 +1,32 @@
 """Count words in file."""
-from symbol import compound_stmt
+import sys
+filename = sys.argv[1]
 
+word_count = {}
 
-input_file = open("test.txt")
-word_counts = {}
+def normalize_word(word):
+    word = word.lower()
+    normal_word = ""
+    acceptable_chars = 'abcdefghijklmnopqrstuvwxyz'
+    acceptable_chars = acceptable_chars + "'" + "-"
+    for char in word:
+        if char in acceptable_chars:
+            normal_word = normal_word + char
+    return normal_word
 
-for line in input_file:
-    line = line.rstrip()
-    words = line.split()
+def generate_word_count(filename = filename, word_count = word_count):
+    """modifies dictionary 'word_count' to count words in filename"""
+
+    word_count = {}
+
+    for line in open(filename):
+        line = line.rstrip()
+        words = line.split(' ')
+        for word in words:
+            word = normalize_word(word)
+            word_count[word] = word_count.get(word,0) + 1
     
-    for word in words:
-        word_counts[word]= word_counts.get(word,0)+1
-    
-for word, count in word_counts.items():
-    print(word, count)
-    
-    
+    for word, count in word_count.items():
+        print(f"{word} {count}")
 
-
-# put your code here.
+generate_word_count()
